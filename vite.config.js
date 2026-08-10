d09993986d5f5ab3caaacf551456e31a376f5bf4
TorbenBachSvendsen <202405610@post.au.dk>
2026-08-10 22:09:56 +0200

    hopefully correct base this time

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/Lord-Yuzuchip/Lord-Yuzuchip.github.io/',
+  base: '/Lord-Yuzuchip.github.io',
   plugins: [
     react(),
     tailwindcss(),
